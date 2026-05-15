7316e3e1ad762e255a4cb4d9bde15e5b6c0ee1ef
wtarit <60599564+wtarit@users.noreply.github.com>
2026-05-14 20:44:21 -0700

    Update DB to require SSL

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
-engine = create_async_engine(settings.database_url, echo=False)
+engine = create_async_engine(
+    settings.database_url, echo=False, connect_args={"ssl": "require"}
+)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
